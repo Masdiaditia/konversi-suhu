b8310346c18725c14e35f91216c4bbdc4b69515a
Masdi Aditia <45525790+Masdiaditia@users.noreply.github.com>
2018-12-23 15:54:01 +0700

    Add files via upload

---
+++ Konversi Suhu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float C,F,R;
+    cout<<"Program konversi suhu"<<endl;
+    cout<<"kanggahani.com"<<endl<<endl;
+    cout<<"masukkan nilai celciusnya= ";
+    cin>>C;
+    F=C*1.8+32;
+    R=C*0.8;
+    cout<<" nilai fahrent= "<<F<<endl<<"nilai Remur ="<<R;
+    return 0;
+
+}
